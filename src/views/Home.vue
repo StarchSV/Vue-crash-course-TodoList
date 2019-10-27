cf62e2c2899fa5395b2bed81299bbedf03b166d8
StarchSV <serjstarchenko@gmail.com>
2019-10-27 14:43:44 +0300

    result

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum soluta vel maiores reiciendis eligendi, ad culpa blanditiis est beatae. Soluta optio corrupti laudantium voluptates rem eligendi consectetur facere ipsa repellat.</p>
+
+    <a href="/todos">Todos</a>
+  </div>
+</template>
